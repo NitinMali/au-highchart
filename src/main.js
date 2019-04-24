01140ffef0842520c90628d2fb1d19593812d2d3
Nitin Mali <nitinmali22@gmail.com>
2019-04-24 14:20:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,28 +16,3 @@ export function configure(aurelia) {
 
   aurelia.start().then(() => aurelia.setRoot());
 }
-/*
-function initialize() {
-  WebCC.Extensions.HMI.DomainLogic.onDLSendData.subscribe(getCurrentLanguage);
-  WebCC.Extensions.HMI.DomainLogic.sendDLEvent(109, {});
-}
-
-function interfaceProperties() {
-  let Interface = {
-    properties: {}
-  };
-  return Interface;
-}
-
-WebCC.start(
-  function(result) {
-    if (result) {
-      initialize();
-      interfaceProperties();
-    } else {
-      console.log('Initialization error');
-    }
-
-    aurelia.setRoot();
-  }, interfaceProperties(), ['HMI', 'Dialogues'], 10000);
-  */
